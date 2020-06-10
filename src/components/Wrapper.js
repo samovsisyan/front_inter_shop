434b6f85ebd522fe733f04c7e005029ec20ffbde
samovsisyan <samovsisyan9@gmail.com>
2020-06-10 17:32:43 +0400

    Create Header Html

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import Header from "./Header";
+import Footer from "./Footer";
 
 class Wrapper extends Component {
     render() {
         return (
             <div>
-                
+                <Header/>
+                {this.props.children}
+                <Footer/>
             </div>
         );
     }
