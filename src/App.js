7fcf2b801c652d0eca063e6e00c1fa30f0ef0b1e
samovsisyan <samovsisyan9@gmail.com>
2020-06-10 18:50:03 +0400

    Crate pages-Category

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import AboutUs from "./pages/AboutUs";
+import Category from "./pages/Category";
 
 class App extends Component {
     render() {
@@ -13,6 +15,10 @@ class App extends Component {
                     <Route path="/" exact component={Home}/>
                     <Route path="/header" component={Header} />
                     <Route path="/footer" component={Footer} />
+                    <Route path="/about-us" component={AboutUs}/>
+                    <Route path="/Category" component={Category}/>
+
+
                 </Switch>
             </BrowserRouter>
         );
